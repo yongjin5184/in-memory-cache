4463d3dd7b12a6edf071ed0b5b5a0f3d04ba1706
Kimyongjin <yongjin5184@gmail.com>
2020-12-17 11:54:52 +0900

    CategoryRepository 와 ProductRepository 를 추가하라

---
+++ src/main/java/com/mall/commerce/repository/CategoryRepository.java
@@ -0,0 +1,14 @@
+package com.mall.commerce.repository;
+
+import com.mall.commerce.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findAllByParentNo(int parentNo);
+
+    Category findByCategoryName(String categoryName);
+}
---
+++ src/main/java/com/mall/commerce/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.mall.commerce.repository;
+
+import com.mall.commerce.entity.Category;
+import com.mall.commerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAllProductsByCategory(Category category);
+}
