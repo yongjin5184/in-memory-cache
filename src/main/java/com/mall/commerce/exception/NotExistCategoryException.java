0b8499008cbe16a706e3ab9d5c1af14295f84254
Kimyongjin <yongjin5184@gmail.com>
2020-12-22 09:56:11 +0900

    특정 카테고리 없을 떄, Exception 추가

---
+++ src/main/java/com/mall/commerce/exception/NotExistCategoryException.java
@@ -0,0 +1,7 @@
+package com.mall.commerce.exception;
+
+public class NotExistCategoryException extends RuntimeException {
+    public NotExistCategoryException() {
+        super("존재하지 않는 카테고리입니다.");
+    }
+}
